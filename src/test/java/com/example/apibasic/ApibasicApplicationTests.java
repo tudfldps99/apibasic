84fc2bfc1f493a965e875eff5920ceead6e08703
tudfldps99 <tudfldps99@naver.com>
2023-01-16 11:56:01 +0900

    # database connect configuration (mariadb)

--- src/test/java/com/example/apibasic/ApibasicApplicationTests.java
+++ src/test/java/com/example/apibasic/ApibasicApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.apibasic;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class ApibasicApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 
+	@Test
+	void dbConnectTest() {
+		String sql = "SELECT NOW() AS now FROM dual";
+		String now = jdbcTemplate.queryForObject(sql, String.class);
+
+		System.out.println("now = " + now);
+	}
 }
